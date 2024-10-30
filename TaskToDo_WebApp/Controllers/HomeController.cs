acf92587f7ee7762cce867b59e51b8d6ed6d216f
Bartosz O. <bartosz.osmanski.external@eviden.com>
2024-10-30 08:45:32 +0100

    removed ilogger

--- TaskToDo_WebApp/Controllers/HomeController.cs
+++ TaskToDo_WebApp/Controllers/HomeController.cs
@@ -4,13 +4,6 @@ namespace TaskToDo_WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
