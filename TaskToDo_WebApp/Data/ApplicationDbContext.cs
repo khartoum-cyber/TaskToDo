5508ab596172f00bacfdb7f7029196457591a627
Bartosz O. <bartosz.osmanski.external@eviden.com>
2024-11-12 11:31:01 +0100

    seeded DB with 2 default tasks

--- TaskToDo_WebApp/Data/ApplicationDbContext.cs
+++ TaskToDo_WebApp/Data/ApplicationDbContext.cs
@@ -6,6 +6,20 @@ namespace TaskToDo_WebApp.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<ToDoTask> Tasks { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var taskList = new[]
+            {
+                new ToDoTask { TaskId = 1, Name = "Go to work", DateAdded = DateTime.Parse("1984-3-13") },
+                new ToDoTask { TaskId = 2, Name = "Clean house", DateAdded = DateTime.Parse("1959-4-15") },
+            };
+
+            modelBuilder.Entity<ToDoTask>().HasData(taskList);
+        }
     }
 }
